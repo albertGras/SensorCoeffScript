082d4b6a2f2f4cbc17c43b0685d1b1d661326d33
Allison Grasmeder <allison@gras.org>
2019-01-25 12:17:24 -0700

    Added in red er doc for reading and comparison

--- scriptUtility.py
+++ scriptUtility.py
@@ -38,9 +38,9 @@ def tableSetUp(line):
     
     
 def copyCodeFile(sensor_list, coeff_title_list, coeff_table, cat_title_list, cat_table, smv_title_list, smv_table, constants_table):
-#    code_file   = open("H:\SensorScript\practice.h")
+    code_file   = open("H:\SensorScript\practice.h")
 #    code_file   = open("C:\PVCS\ProjectsDB\Kinetis_DB\k2Src\k_src_app\coriolis\sensor.cpp")
-    code_file = open(r"C:\Users\AGrasmeder\Documents\SensorCoeffScript\practice.h")
+#    code_file = open(r"C:\Users\AGrasmeder\Documents\SensorCoeffScript\practice.h")
 
     inCoeffTable = False
     inSmvTable = False
@@ -156,8 +156,8 @@ def copyExcelFile(excel_title_list_one, excel_table_one, excel_title_list_two, e
     # Copy all coefficient names in blue er doc - old params 
     num_cols = sheet_one.ncols   # Number of columns
     for col_idx in range(0, num_cols):  # Iterate through columns
-        excel_title_list_two.append(sheet_one.cell_value(title_line_num, col_idx))  # Get cell object by row, col
-#    print (excel_title_list_two)
+        excel_title_list_one.append(sheet_one.cell_value(title_line_num, col_idx))  # Get cell object by row, col
+#    print (excel_title_list_one)
 #    print()
 
     # Copy all values in blue er doc - old params 
@@ -166,18 +166,15 @@ def copyExcelFile(excel_title_list_one, excel_table_one, excel_title_list_two, e
             new_row.append(sheet_one.cell_value(row_idx, col_idx))  # Get cell object by row, col
 #        print (new_row)
 #        print()
-        excel_table_two.append(new_row)
+        excel_table_one.append(new_row)
         new_row = []
-
-
 
     # Copy all coefficient names in blue er doc - new params 
     num_cols = sheet_two.ncols   # Number of columns
     for col_idx in range(0, num_cols):  # Iterate through columns (x axis direction)
-        excel_title_list_one.append(sheet_two.cell_value(title_line_num, col_idx))  # Get cell object by row, col
-#    print (excel_title_list_one)
+        excel_title_list_two.append(sheet_two.cell_value(title_line_num, col_idx))  # Get cell object by row, col
+#    print (excel_title_list_two)
 #    print()
-
 
     # Copy all values in blue er doc - new params 
     for row_idx in range(data_line_num, sheet_two.nrows):    # Iterate through rows (y axis direction)
@@ -185,26 +182,35 @@ def copyExcelFile(excel_title_list_one, excel_table_one, excel_title_list_two, e
             new_row.append(sheet_two.cell_value(row_idx, col_idx))  # Get cell object by row, col
 #        print (new_row)
 #        print()
-        excel_table_one.append(new_row)
+        excel_table_two.append(new_row)
         new_row = []
     
     
-def checkDocForCoeff(doc_title_list, doc_table, coeff, code_row, working_row):
+def checkDocForCoeff(doc_title_list, doc_table, coeff, code_row, working_row, sensor_col):
     for doc_coeff_num, doc_coeff_name in enumerate(doc_title_list, 0): # loop through new sheet doc coeff titles
         if coeff == doc_coeff_name: #If these match then this doc contains the coeff title 
-            for doc_row in doc_table: #loop through each row of the blue doc
-                if code_row[0] in doc_row[3]: # If the sensor in the doc matched the sensor in the code
+            for doc_row in doc_table: #loop through each row of the doc
+                if code_row[0] in str(doc_row[sensor_col]): # If the sensor in the doc matched the sensor in the code
                     working_row.append(str(doc_row[doc_coeff_num]))
                     return working_row
 
-def compileErDocRow(coeff, code_row, working_row, new_blue_doc_title_list, new_blue_doc_table, old_blue_doc_title_list, old_blue_doc_table):
+def compileErDocRow(coeff, code_row, working_row, new_blue_doc_title_list, new_blue_doc_table, old_blue_doc_title_list, old_blue_doc_table,
+    coriolis_red_doc_title_list, coriolis_red_doc_table, dens_visc_red_doc_title_list, dens_visc_red_doc_table):
+    
+    blue_sensor_col = 3
+    red_sensor_col = 1
+    
     # Look at new sheet in blue ER doc for coeff 
-    if checkDocForCoeff(new_blue_doc_title_list, new_blue_doc_table, coeff, code_row, working_row) != None:
+    if checkDocForCoeff(new_blue_doc_title_list, new_blue_doc_table, coeff, code_row, working_row, blue_sensor_col) != None:
         return
     # Look at old sheet in blue ER doc for coeff
-    if checkDocForCoeff(old_blue_doc_title_list, old_blue_doc_table, coeff, code_row, working_row) != None:
+    if checkDocForCoeff(old_blue_doc_title_list, old_blue_doc_table, coeff, code_row, working_row, blue_sensor_col) != None:
         return
-        
+    if checkDocForCoeff(coriolis_red_doc_title_list, coriolis_red_doc_table, coeff, code_row, working_row, red_sensor_col) != None:
+        return
+    if checkDocForCoeff(dens_visc_red_doc_title_list, dens_visc_red_doc_table, coeff, code_row, working_row, red_sensor_col) != None:
+        return
+
     working_row.append('---') # Else, add a '---' for coefficients that cant be populated
     return working_row
 
@@ -218,7 +224,8 @@ def formatString(stringVariable):
 
 
 def createFinalArray(final_array, coeffs_to_compare, coeff_table, coeff_title_list, working_row, final_code_array, new_blue_doc_title_list,
-new_blue_doc_table, old_blue_doc_title_list, old_blue_doc_table, final_doc_array):
+    new_blue_doc_table, old_blue_doc_title_list, old_blue_doc_table, coriolis_red_doc_title_list, coriolis_red_doc_table, 
+    dens_visc_red_doc_title_list, dens_visc_red_doc_table, final_doc_array):
 
     coeffPopulated = False
 
@@ -236,9 +243,6 @@ new_blue_doc_table, old_blue_doc_title_list, old_blue_doc_table, final_doc_array
 #                    print(code_row[code_coeff_num])
             if coeffPopulated == False:
                 working_row.append("---")  # Add a place holder if value doesnt exist
-#                print("---")
-#                print("End code")
-#            print()
         final_code_array.append(working_row)
 #        print(working_row)
         working_row = []
@@ -247,7 +251,8 @@ new_blue_doc_table, old_blue_doc_title_list, old_blue_doc_table, final_doc_array
         
         # put er document coeffs into final array 
         for coeff in coeffs_to_compare:  # Loop through master coeff list
-            compileErDocRow(coeff, code_row, working_row, new_blue_doc_title_list, new_blue_doc_table, old_blue_doc_title_list, old_blue_doc_table)
+            compileErDocRow(coeff, code_row, working_row, new_blue_doc_title_list, new_blue_doc_table, old_blue_doc_title_list, old_blue_doc_table,
+                coriolis_red_doc_title_list, coriolis_red_doc_table, dens_visc_red_doc_title_list, dens_visc_red_doc_table)
         final_doc_array.append(working_row)
 #        print (working_row)
 #        print()
@@ -283,8 +288,8 @@ def exportFinalArraytoExcelDocument(final_array):
     #Transfer final array values to excel document 
 
     # Create an new Excel file and add a worksheet.
-#    workbook = xlsxwriter.Workbook('H:\SensorScript\demo.xlsx')
-    workbook = xlsxwriter.Workbook(r'C:\Users\AGrasmeder\Documents\SensorCoeffScript')
+    workbook = xlsxwriter.Workbook('H:\SensorScript\demo.xlsx')
+#    workbook = xlsxwriter.Workbook(r'C:\Users\AGrasmeder\Documents\SensorCoeffScript')
     worksheet = workbook.add_worksheet()
 
     # Widen the first column to make the text clearer.
--- sensorCoeffScript.py
+++ sensorCoeffScript.py
@@ -2,16 +2,17 @@ from scriptUtility import *
 
 
 print("Hello World!")
+print()
 
 #All the coeffs that need comparison
-coeffs_to_compare = ["ID String", "FCF", "K1", "GasFD", "NominalFlowRate", "TubeID", "A4"]
-#coeffs_to_compare = ["ID String", "NominalFlowRate", "FCF", "K1", "I.D. Resistor", "Drive Target", "Proportional Gain800", 
-#    "Integral Gain800", "Proportional Gain2200", "Integral Gain2200", "Proportional Gain700", "Integral Gain700", 
-#    "FD Limit", "Overshoot", "GasFD", "TubeID", "mp,   p < 0.4", "bp,   p < 0.4", "mp,   p > 0.4", "bp,   p > 0.4",
-#    "DriveSaturationAlgorithm800", "PressureEffect_Flow_Liquid", "PressureEffect_Flow_Gas", "PressureEffect_Density", 
-#    "Zero Stability", "A4", "TemperatureEffect_Density", "TemperatureEffect_Flow", "Tone Level", "Ramp Time", 
-#    "BL Temp Coeff*", "Drive SP FCF", "Puck P FCF", "dF Tone Spacing", "Freq. Drift Limit*", "Max Sensor Current", 
-#    "Minimum Flow Multiplier", "T03", "MassFlowAccuracy_Liquid", "MassFlowAccuracyMVD_Gas", "DensityAccuracy_Liquid", ] 
+#coeffs_to_compare = ["ID String", "FCF", "K1", "GasFD", "NominalFlowRate", "TubeID", "A4"]
+coeffs_to_compare = ["ID String", "NominalFlowRate", "FCF", "K1", "I.D. Resistor", "Drive Target", "Proportional Gain800", 
+    "Integral Gain800", "Proportional Gain2200", "Integral Gain2200", "Proportional Gain700", "Integral Gain700", 
+    "FD Limit", "Overshoot", "GasFD", "TubeID", "mp,   p < 0.4", "bp,   p < 0.4", "mp,   p > 0.4", "bp,   p > 0.4",
+    "DriveSaturationAlgorithm800", "PressureEffect_Flow_Liquid", "PressureEffect_Flow_Gas", "PressureEffect_Density", 
+    "Zero Stability", "A4", "TemperatureEffect_Density", "TemperatureEffect_Flow", "Tone Level", "Ramp Time", 
+    "BL Temp Coeff*", "Drive SP FCF", "Puck P FCF", "dF Tone Spacing", "Freq. Drift Limit*", "Max Sensor Current", 
+    "Minimum Flow Multiplier", "T03", "MassFlowAccuracy_Liquid", "MassFlowAccuracyMVD_Gas", "DensityAccuracy_Liquid", ] 
 
 #Lists for copying the code file
 sensor_list = [] #All the sensors listed in the code
@@ -24,8 +25,8 @@ smv_table = [] #Table of all Smart Meter Verification coefficients
 constants_table = []
 
 #Blue ER document variables
-#blue_excel_file   = "H:\SensorScript\ER docs\ER-20018334_AK.xlsx" #Read in blue ER document into an array
-blue_excel_file   = r"C:\Users\AGrasmeder\Documents\SensorCoeffScript\ER docs\ER-20018334_AK.xlsx"
+blue_excel_file   = "H:\SensorScript\ER docs\ER-20018334_AK.xlsx" #Read in blue ER document into an array
+#blue_excel_file   = r"C:\Users\AGrasmeder\Documents\SensorCoeffScript\ER docs\ER-20018334_AK.xlsx"
 blue_excel_title_line_num = 15 # The row number of where the title names are located
 blue_excel_data_line_num = 21 # The row number of where the coeff data begins
 new_blue_doc_title_list = []
@@ -34,8 +35,8 @@ old_blue_doc_title_list =[]
 old_blue_doc_table =[]
 
 #Red ER document variables
-#red_excel_file   = "H:\SensorScript\ER docs\ER-20015860_CF.xlsx" #Read in red ER document into an array
-red_excel_file   = r"C:\Users\AGrasmeder\Documents\SensorCoeffScript\ER docs\ER-20015860_CF.xlsx"
+red_excel_file   = "H:\SensorScript\ER docs\ER-20015860_CF.xlsx" #Read in red ER document into an array
+#red_excel_file   = r"C:\Users\AGrasmeder\Documents\SensorCoeffScript\ER docs\ER-20015860_CF.xlsx"
 red_excel_title_line_num = 6 # The row number of where the title names are located
 red_excel_data_line_num = 10 # The row number of where the coeff data begins
 coriolis_red_doc_title_list = []
@@ -62,19 +63,31 @@ replaceVariablesWithDefinitions(coeff_table, constants_table)
 
 
 #ER-20018334  /  Blue
-copyExcelFile(new_blue_doc_title_list, new_blue_doc_table, old_blue_doc_title_list, old_blue_doc_table, blue_excel_file, 
+copyExcelFile(old_blue_doc_title_list, old_blue_doc_table, new_blue_doc_title_list, new_blue_doc_table, blue_excel_file, 
     blue_excel_title_line_num, blue_excel_data_line_num) 
 
 #ER-20015860  /  Red
-copyExcelFile(coriolis_red_doc_title_list, coriolis_red_doc_table, dens_visc_red_doc_title_list, dens_visc_red_doc_table, red_excel_file, 
-    red_excel_title_line_num, red_excel_data_line_num) 
+copyExcelFile(coriolis_red_doc_title_list, coriolis_red_doc_table, dens_visc_red_doc_title_list, dens_visc_red_doc_table, 
+    red_excel_file, red_excel_title_line_num, red_excel_data_line_num) 
+    
+#print(dens_visc_red_doc_title_list)
+#print()
+#print(dens_visc_red_doc_table)
+#print()
+#print()
+#print(coriolis_red_doc_title_list)
+#print()
+#print(coriolis_red_doc_table)
+#print()
+#print()
 
 
 
 createFinalArray(final_array, coeffs_to_compare, coeff_table, coeff_title_list, working_row, final_code_array, new_blue_doc_title_list,
-    new_blue_doc_table, old_blue_doc_title_list, old_blue_doc_table, final_doc_array)
+    new_blue_doc_table, old_blue_doc_title_list, old_blue_doc_table, coriolis_red_doc_title_list, coriolis_red_doc_table, 
+    dens_visc_red_doc_title_list, dens_visc_red_doc_table, final_doc_array)
 
-#exportFinalArraytoExcelDocument(final_array)
+exportFinalArraytoExcelDocument(final_array)
 
 
 
